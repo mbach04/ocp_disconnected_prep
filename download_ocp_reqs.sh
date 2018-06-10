51f7011a88907a34074c7ee272b197fdd4522606
mbach04 <mbach04@gmail.com>
2018-06-10 18:03:41 -0400

    add quay role

--- download_ocp_reqs.sh
+++ download_ocp_reqs.sh
@@ -4,20 +4,18 @@
 ### Note! Run as root user
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
 
-echo "Enter RHN User:/n"
+echo "Enter RHN User:"
 read RHN_USER
-echo "Enter RHN Password:/n"
+echo "Enter RHN Password:"
 read -s RHN_PASSWORD
-echo "Enter full path to storage location for export files (requires ~130 GB of available space):/n"
+echo "Enter full path to storage location for export files (requires ~130 GB of available space):"
 read STORAGE_PATH
 
 IMAGE_STORAGE_PATH=$STORAGE_PATH/images
 REPO_STORAGE_PATH=$STORAGE_PATH/repo_files
 
-# STORAGE_SIZE=$(df --output=size /mnt/xyz | awk ' NR==2 ')
-# MIN_SIZE=136314880
-
-
+mkdir -p $IMAGE_STORAGE_PATH
+mkdir -p $REPO_STORAGE_PATH
 
 subscription-manager register --username=$RHN_USER --password=$RHN_PASSWORD
 subscription-manager refresh
@@ -34,12 +32,21 @@ subscription-manager repos \
     --enable="rhel-7-server-ansible-2.4-rpms" \
     --enable="rhel-7-server-ose-3.9-rpms"
 
-yum -y install yum-utils createrepo docker git
+yum -y install yum-utils createrepo docker git wget
+
+#epel
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install ./epel-release-latest-*.noarch.rpm -y
+
+yum install ntfs-3g -y
+mount -t ntfs-3g /dev/sdb1 /media/rhel_exports
+
+#start/enable docker
 systemctl enable docker
 systemctl start docker
 
-mkdir -p $IMAGE_STORAGE_PATH
-mkdir -p $REPO_STORAGE_PATH
+
+
 
 
 for repo in \
@@ -54,60 +61,61 @@ do
 done
 
 clear
-echo "Finished downloading repo files: your files are located in /tmp/repos"
+echo "Finished downloading repo files"
+echo "Starting container image downloads"
 sleep 3
 
-echo "Loggin into registry.access.redhat.com with docker..."
+echo "Logging into registry.access.redhat.com with docker..."
 docker login registry.access.redhat.com -u $RHN_USER -p $RHN_PASSWORD
 
 
 declare -a images=(
-        "openshift3/ose-ansible:v3.9.27"
-        "openshift3/ose-cluster-capacity:v3.9.27"
-        "openshift3/ose-deployer:v3.9.27"
-        "openshift3/ose-docker-builder:v3.9.27"
-        "openshift3/ose-docker-registry:v3.9.27"
-        "openshift3/ose-egress-http-proxy:v3.9.27"
-        "openshift3/ose-egress-router:v3.9.27"
-        "openshift3/ose-f5-router:v3.9.27"
-        "openshift3/ose-haproxy-router:v3.9.27"
-        "openshift3/ose-keepalived-ipfailover:v3.9.27"
-        "openshift3/ose-pod:v3.9.27"
-        "openshift3/ose-sti-builder:v3.9.27"
-        "openshift3/ose:v3.9.27"
-        "openshift3/container-engine:v3.9.27"
-        "openshift3/node:v3.9.27"
-        "openshift3/openvswitch:v3.9.27"
+        "openshift3/ose-ansible:v3.9"
+        "openshift3/ose-cluster-capacity:v3.9"
+        "openshift3/ose-deployer:v3.9"
+        "openshift3/ose-docker-builder:v3.9"
+        "openshift3/ose-docker-registry:v3.9"
+        "openshift3/ose-egress-http-proxy:v3.9"
+        "openshift3/ose-egress-router:v3.9"
+        "openshift3/ose-f5-router:v3.9"
+        "openshift3/ose-haproxy-router:v3.9"
+        "openshift3/ose-keepalived-ipfailover:v3.9"
+        "openshift3/ose-pod:v3.9"
+        "openshift3/ose-sti-builder:v3.9"
+        "openshift3/ose:v3.9"
+        "openshift3/container-engine:v3.9"
+        "openshift3/node:v3.9"
+        "openshift3/openvswitch:v3.9"
         "rhel7/etcd"
         "openshift3/ose-recycler"
-        "openshift3/logging-curator:v3.9.27"
-        "openshift3/logging-auth-proxy:v3.9.27"
-        "openshift3/logging-elasticsearch:v3.9.27"
-        "openshift3/logging-fluentd:v3.9.27"
-        "openshift3/logging-kibana:v3.9.27"
-        "openshift3/metrics-cassandra:v3.9.27"
-        "openshift3/metrics-hawkular-metrics:v3.9.27"
-        "openshift3/metrics-hawkular-openshift-agent:v3.9.27"
-        "openshift3/metrics-heapster:v3.9.27"
-        "openshift3/prometheus:v3.9.27"
-        "openshift3/prometheus-alert-buffer:v3.9.27"
-        "openshift3/prometheus-alertmanager:v3.9.27"
-        "openshift3/prometheus-node-exporter:v3.9.27"
-        "cloudforms46/cfme-openshift-postgresql:v3.9.27"
-        "cloudforms46/cfme-openshift-memcached:v3.9.27"
-        "cloudforms46/cfme-openshift-app-ui:v3.9.27"
-        "cloudforms46/cfme-openshift-app:v3.9.27"
-        "cloudforms46/cfme-openshift-embedded-ansible:v3.9.27"
-        "cloudforms46/cfme-openshift-httpd:v3.9.27"
-        "cloudforms46/cfme-httpd-configmap-generator:v3.9.27"
-        "rhgs3/rhgs-server-rhel7:v3.9.27"
-        "rhgs3/rhgs-volmanager-rhel7:v3.9.27"
-        "rhgs3/rhgs-gluster-block-prov-rhel7:v3.9.27"
-        "rhgs3/rhgs-s3-server-rhel7:v3.9.27"
-        "openshift3/ose-service-catalog:v3.9.27"
-        "openshift3/ose-ansible-service-broker:v3.9.27"
-        "openshift3/mediawiki-apb:v3.9.27"
-        "openshift3/postgresql-apb:v3.9.27"
+        "openshift3/logging-curator:v3.9"
+        "openshift3/logging-auth-proxy:v3.9"
+        "openshift3/logging-elasticsearch:v3.9"
+        "openshift3/logging-fluentd:v3.9"
+        "openshift3/logging-kibana:v3.9"
+        "openshift3/metrics-cassandra:v3.9"
+        "openshift3/metrics-hawkular-metrics:v3.9"
+        "openshift3/metrics-hawkular-openshift-agent:v3.9"
+        "openshift3/metrics-heapster:v3.9"
+        "openshift3/prometheus:v3.9"
+        "openshift3/prometheus-alert-buffer:v3.9"
+        "openshift3/prometheus-alertmanager:v3.9"
+        "openshift3/prometheus-node-exporter:v3.9"
+        "cloudforms46/cfme-openshift-postgresql:latest"
+        "cloudforms46/cfme-openshift-memcached:latest"
+        "cloudforms46/cfme-openshift-app-ui:latest"
+        "cloudforms46/cfme-openshift-app:latest"
+        "cloudforms46/cfme-openshift-embedded-ansible:latest"
+        "cloudforms46/cfme-openshift-httpd:latest"
+        "cloudforms46/cfme-httpd-configmap-generator:latest"
+        "rhgs3/rhgs-server-rhel7:v3.9"
+        "rhgs3/rhgs-volmanager-rhel7:v3.9"
+        "rhgs3/rhgs-gluster-block-prov-rhel7:v3.9"
+        "rhgs3/rhgs-s3-server-rhel7:v3.9"
+        "openshift3/ose-service-catalog:v3.9"
+        "openshift3/ose-ansible-service-broker:v3.9"
+        "openshift3/mediawiki-apb:v3.9"
+        "openshift3/postgresql-apb:v3.9"
         "rhscl/nodejs-4-rhel7"
         "rhscl/nodejs-6-rhel7"
         "rhscl/ruby-22-rhel7"
@@ -140,12 +148,12 @@ declare -a images=(
         "redhat-openjdk-18/openjdk18-openshift"
         "redhat-sso-7/sso71-openshift"
         "redhat-sso-7/sso72-openshift"
-        "openshift3/registry-console:v3.9.27"
-        "openshift3/jenkins-1-rhel7:v3.9.27"
-        "openshift3/jenkins-2-rhel7:v3.9.27"
-        "openshift3/jenkins-slave-base-rhel7:v3.9.27"
-        "openshift3/jenkins-slave-maven-rhel7:v3.9.27"
-        "openshift3/jenkins-slave-nodejs-rhel7:v3.9.27"
+        "openshift3/registry-console:v3.9"
+        "openshift3/jenkins-1-rhel7:v3.9"
+        "openshift3/jenkins-2-rhel7:v3.9"
+        "openshift3/jenkins-slave-base-rhel7:v3.9"
+        "openshift3/jenkins-slave-maven-rhel7:v3.9"
+        "openshift3/jenkins-slave-nodejs-rhel7:v3.9"
         "rhscl/postgresql-96-rhel7:latest"
         "openshift3/oauth-proxy:latest"
 )
@@ -156,15 +164,19 @@ echo "Pulling docker images now..."
 for i in "${images[@]}"
 do
    docker pull registry.access.redhat.com/$i
+   echo "$i $?" >> $IMAGE_STORAGE_PATH/download_results.log 
 done
 
 # Grab nexus image
+docker login registry.connect.redhat.com -u $RHN_USER -p $RHN_PASSWORD
 docker pull registry.connect.redhat.com/sonatype/nexus-repository-manager
+echo "sonatype/nexus-repository-manager $?" >> $IMAGE_STORAGE_PATH/download_results.log
 
 # download coreos images
 docker pull quay.io/quay/redis:latest
+echo "quay.io/quay/redis $?" >> $IMAGE_STORAGE_PATH/download_results.log
 docker pull quay.io/coreos/quay:v2.9.1
-
+echo "quay.io/coreos/quay $?" >> $IMAGE_STORAGE_PATH/download_results.log
 
 
 # Save the images
@@ -179,6 +191,6 @@ echo "Downloading infra-ansible roles..."
 git clone https://github.com/redhat-cop/infra-ansible.git $REPO_STORAGE_PATH/quay-role
 
 
-echo "Finished/n"
-echo "Your files are located in $STORAGE_PATH. Please export the entirety of $STORAGE_PATH onto an external disk."
+echo "Finished"
+echo "Your files are located in $STORAGE_PATH."
 
